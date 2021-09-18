201648f2158c0ef83fd83946ac5590ccca15417e
Andrew Li <andrew.champ.li@gmail.com>
2021-09-18 16:21:32 -0400

    added api functions

---
+++ backend/api/usermanager.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+def get_player_id(summoner_name: str, region: str, key: str) -> str:
+    return ""
+
+
+def get_recent_matches(pid: int, region: str, key: str) -> List[str]:
+    return []
