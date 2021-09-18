cad3be2717e0a3c323836279d0031fa877a0c14e
MinisGimis <81587461+MinisGimis@users.noreply.github.com>
2021-09-18 16:22:23 -0400

    Update models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,7 @@
 from . import db 
 
-class Movie(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(50))
-    rating = db.Column(db.Integer)
+class Summoner(db.Model):
+    player_id = db.Column(db.String(100), primary_key=True)
+    summoner_name = db.Column(db.String(20))
+    region = db.Column(db.String(10))
+    
